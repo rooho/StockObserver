635e616062db25845dab4f1f8c1112581f562128
yanjie.zhou <yanjie@rooho.cc>
2019-06-03 23:41:35 +0800

    Feat. add Element-ui.

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
-import 'bulma-fluent/bulma.sass'
-import iView from 'iview';
 
 import App from './pages/App'
-
-// 加入 iView 组件
-Vue.use(iView);
 
 // eslint-disable-next-line
 new Vue({
--- src/popup/pages/App.vue
+++ src/popup/pages/App.vue
@@ -1,26 +1,17 @@
 <template>
     <div>
         <h1>{{ msg }}</h1>
-        <span> page list </span> Correct usage:
-        <Page :current="1" :total="100"></Page>
-    
-        Incorrect usage:
-        <Page current="1" total="100"></Page>
+        <span> page list </span>
     </div>
 </template>
 
 <script>
-import { Button, Table } from 'iview';
 
 export default {
     data() {
         return {
             msg: 'Welcome baby!'
         }
-    },
-    components: {
-        "i-button": Button,
-        "i-table": Table,
     },
 }
 </script>
