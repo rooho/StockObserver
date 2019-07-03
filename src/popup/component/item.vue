7d7a1c7f237600c1aa0077c0616f4ceef9308bb0
rooho <rooho.s.co@gmail.com>
2019-07-03 13:50:20 +0800

    修改列表展示。

--- src/popup/component/item.vue
+++ src/popup/component/item.vue
@@ -4,20 +4,6 @@
       #{{ index }}
     </div>
     <div class="card">
-      <div class="card-avatar"
-           :class="{'no-avatar': !avatar}"
-           :style="{'background-color': !avatar && info.color}">
-        <span v-if="!avatar">{{ getAbbrName(info.name) }}</span>
-        <img v-else class="card-avatar-img" :src="info.avatar" alt="AVATAR">
-      </div>
-      <div class="card-info">
-        <div class="card-info-item name" :style="{color: info.color}">
-          {{ info.name }}
-        </div>
-        <div class="card-info-item time">
-          Registered: {{ info.time }}
-        </div>
-      </div>
       <div v-if="variable" class="card-height">
         {{ height }}px
       </div>
@@ -80,93 +66,9 @@
   .item {
     box-sizing: border-box;
     display: flex;
-    @media (max-width: 640px) {
-      -webkit-user-select: none;
-      user-select: none;
-    }
-
-    .index {
-      flex: 1;
-      text-align: center;
-    }
-
+    height: 100px;
     .card {
-      position: relative;
-      flex: 4;
-      display: flex;
-      align-items: center;
-      border-bottom: 1px dashed #cecece;
-      @media (max-width: 640px) and (-webkit-min-device-pixel-ratio: 2) {
-        border-bottom: 0.5px solid #cccccc;
-      }
-
-      &-avatar {
-        width: 40px;
-        height: 40px;
-        background: #efefef;
-        color: #4169e1;
-        border-radius: 50%;
-        text-align: center;
-        line-height: 40px;
-
-        &.no-avatar {
-          background: #ff6347;
-          color: #ffffff;
-        }
-
-        &-img {
-          display: block;
-          width: 100%;
-          height: 100%;
-          border-radius: 50%;
-        }
-      }
-
-      &-info {
-        display: flex;
-        flex-direction: column;
-        // height: 100%;
-        padding-left: 40px;
-        @media (max-width: 640px) {
-          padding-left: 20px;
-        }
-
-        &-item {
-          flex: 1;
-          height: 50%;
-          line-height: 1;
-          position: relative;
-          white-space: nowrap;
-          text-overflow: ellipsis;
-          max-width: 300px;
-          overflow: hidden;
-          @media (max-width: 640px) {
-            max-width: 180px;
-          }
-
-          &.name {
-            padding-bottom: 3px;
-          }
-
-          &.time {
-            padding-top: 3px;
-            color: #a9a9a9;
-          }
-        }
-      }
-
-      &-height {
-        position: absolute;
-        right: 30px;
-        font-style: italic;
-        color: #999;
-        font-weight: 100;
-        font-family: sans-serif;
-        font-size: 12px;
-        @media (max-width: 375px) {
-          right: 10px;
-        }
-      }
+      height: 50px;
     }
   }
 </style>
--- src/popup/pages/App.vue
+++ src/popup/pages/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
-    <span> page list </span>
+    <span> page listA </span>
     <virtual-list class="list"
                   :size="itemSize"
                   :remain="remain"
-                  :bench="30"
+                  :bench="20"
                   :start="start"
-                  :variable="getVariableHeight"
+
                   :item="item"
                   :itemcount="itemCount"
                   :itemprops="getItemProps"
@@ -18,27 +18,44 @@
 <script>
   import stock_item from '../component/item'
   import virtualList from 'vue-virtual-scroll-list'
+  import { countStorage, getRandomUser } from '../component/util'
+
+  // const itemCount = countStorage.get()
 
   export default {
     data() {
       return {
-        msg: 'Welcome baby now!',
+        msg: 'Welcome baby now! yes',
         start: 1,
         remain: 1,
-        itemSize: 80,
-        itemCount: 200,
+        itemCount: 100,
+        itemSize: 180,
         item: stock_item,
+        userInfoList: {},
       }
     },
     methods: {
       loadInitData() {
         console.log("loadInitData!")
         this.insertStorage()
+        for (let i = 0; i < this.itemCount; i += 1) {
+          this.userInfoList.push(getRandomUser())
+        }
       },
       insertStorage() {
         chrome.storage.sync.set({ "yourBody": "myBody" }, function(){
           console.log("insertStorage callback")
         });
+      },
+      getItemProps (itemIndex) {
+        return {
+          key: itemIndex,
+          props: {
+            height: this.itemSize,
+            index: this.itemIndex,
+            info: this.userInfoList[itemIndex] || {}
+          }
+        }
       },
     },
     components: {stock_item, 'virtual-list': virtualList},
